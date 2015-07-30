665305779e6ae8b68d237574d6b72722cd694fd9
ivossss <ivossss@yahoo.com>
2015-07-30 13:05:13 +0300

    Added function for drawing tiles animated.

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,71 @@
 var canvas = document.getElementById('mines-canvas');
-var canvasCtx = canvas.getContext('2d');
+var context = canvas.getContext('2d');
 
 
 function main() {
     var horizontalTiles = 10,
-        verticalTiles = 10,
-        numberOfTiles = horizontalTiles * verticalTiles;
-    drawTiles();
+        verticalTiles = 10;
+    drawTilesAnimated(horizontalTiles, verticalTiles);
+
+    displayNumberOfMines();
 }
 
 function drawTiles(horizontalTiles, verticalTiles) {
     horizontalTiles = horizontalTiles || 10;
     verticalTiles = verticalTiles || 10;
+
     var numberOfTiles = horizontalTiles * verticalTiles,
-        tileSize = 30,
+        tileSize = 40,
         tileStartPositionX = 5,
         tileStartPositionY = tileStartPositionX,
         tileStep = tileSize + tileStartPositionX;
 
-
+    context.fillStyle = 'rgb(107, 187, 201)';
     for (var i = 1; i <= numberOfTiles; i += 1) {
-        canvasCtx.fillRect(tileStartPositionX, tileStartPositionY, tileSize, tileSize);
+        context.fillRect(tileStartPositionX, tileStartPositionY, tileSize, tileSize);
         tileStartPositionX += tileStep;
         if (i % horizontalTiles === 0) {
             tileStartPositionX = 5;
             tileStartPositionY += tileStep;
         }
     }
+}
+
+function drawTilesAnimated(horizontalTiles, verticalTiles) {
+    horizontalTiles = horizontalTiles || 10;
+    verticalTiles = verticalTiles || 10;
+
+    var numberOfTiles = horizontalTiles * verticalTiles,
+        tileSize = 40,
+        tileStartPositionX = 5,
+        tileStartPositionY = tileStartPositionX,
+        tileStep = tileSize + tileStartPositionX,
+        i = 1;
+    
+    function animate() {
+        context.fillStyle = 'rgb(107, 187, 201)';
+        context.fillRect(tileStartPositionX, tileStartPositionY, tileSize, tileSize);
+        tileStartPositionX += tileStep;
+        if (i % horizontalTiles === 0) {
+            tileStartPositionX = 5;
+            tileStartPositionY += tileStep;
+        }
+
+        if (i < numberOfTiles) {
+            setTimeout(animate, 5);
+        }
+
+        i += 1;
+    }
+
+    animate();
+}
+
+
+function displayNumberOfMines() {
+    context.fillStyle = 'black';
+    context.font = '28px Arial';
+    context.fillText('1', 20, 30);
 }
 
 main();
